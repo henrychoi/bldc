4587ef1225ca4e50a60c4d6c5066865f1ab31b4a
Henry <hchoi2@apple.com>
2020-01-19 13:27:27 -0800

    Make MK3 the default target

--- conf_general.h
+++ conf_general.h
@@ -68,7 +68,7 @@
 //#define HW60_VEDDER_FIRST_PCB
 
 // Mark3 version of HW60 with power switch and separate NRF UART.
-//#define HW60_IS_MK3
+#define HW60_IS_MK3
 
 #define HW_SOURCE "hw_60.c"
 #define HW_HEADER "hw_60.h"
@@ -95,7 +95,7 @@
 //#define HW75_300_VEDDER_FIRST_PCB
 
 // Second revision with separate UART for NRF51
-#define HW75_300_REV_2
+//#define HW75_300_REV_2
 
 //#define HW_SOURCE "hw_75_300.c"
 //#define HW_HEADER "hw_75_300.h"
